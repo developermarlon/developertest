f09e4883cd97b5cd79452d21dc72ec840ac9980a
Marlon Torres Lozano <developer.marlon.torres@gmail.com>
2021-05-30 11:56:37 -0500

    update port db

--- src/constans/db.js
+++ src/constans/db.js
@@ -1,7 +1,7 @@
 export default {
     host: process.env.NODE_ENV === 'production' ?  'ec2-52-45-73-150.compute-1.amazonaws.com' : 'localhost',
     database: process.env.NODE_ENV === 'production' ?  'd8dvk7kpnmgu5p' : 'example-crud',
-    port: process.env.NODE_ENV === 'production' ?  '5432' : 3306,
+    port: process.env.NODE_ENV === 'production' ?  5432 : 3306,
     user: process.env.NODE_ENV === 'production' ?  'ahyubmllzzldza' : 'root',
     dialect: process.env.NODE_ENV === 'production' ?  'postgres' : 'mysql',
     password: process.env.NODE_ENV === 'production' ?  'ce12acc9a6baf1103cbe1dbfe55b91e6262358c104060a16ec77cc69786a4b85' : '',
