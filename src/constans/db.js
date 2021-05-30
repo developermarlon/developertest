53d9d0d77e5aaa5eeae05ab7708726954b8462ec
Marlon Torres Lozano <developer.marlon.torres@gmail.com>
2021-05-30 11:16:58 -0500

    comfig database postgres production

--- src/constans/db.js
+++ src/constans/db.js
@@ -1,8 +1,8 @@
 export default {
-    host: process.env.NODE_ENV === 'production' ?  '' : 'localhost',
-    database: process.env.NODE_ENV === 'production' ?  '' : 'example-crud',
-    port: process.env.NODE_ENV === 'production' ?  '' : 3306,
-    user: process.env.NODE_ENV === 'production' ?  '' : 'root',
-    dialect: process.env.NODE_ENV === 'production' ?  '' : 'mysql',
-    password: process.env.NODE_ENV === 'production' ?  '' : '',
+    host: process.env.NODE_ENV === 'production' ?  'ec2-52-45-73-150.compute-1.amazonaws.com' : 'localhost',
+    database: process.env.NODE_ENV === 'production' ?  'd8dvk7kpnmgu5p' : 'example-crud',
+    port: process.env.NODE_ENV === 'production' ?  '5432' : 3306,
+    user: process.env.NODE_ENV === 'production' ?  'ahyubmllzzldza' : 'root',
+    dialect: process.env.NODE_ENV === 'production' ?  'postgres' : 'mysql',
+    password: process.env.NODE_ENV === 'production' ?  'ce12acc9a6baf1103cbe1dbfe55b91e6262358c104060a16ec77cc69786a4b85' : '',
 }
